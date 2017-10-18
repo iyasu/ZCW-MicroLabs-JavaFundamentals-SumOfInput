7bd3975e1218462663e87fec7d3eb8524399ffba
iyasuwatts <iyasuwatts@iyasuwattss-MacBook-Pro.local>
2017-10-18 17:37:53 -0400

    complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
@@ -5,5 +7,22 @@ public class Main {
 
     public static void main(String[] args){
 
+        int userNumber;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        userNumber = in.nextInt();
+
+        System.out.println("The sum of the numbers from 1 to " + userNumber + " is " + sumOfNumbers(userNumber));
+    }
+
+    public static int sumOfNumbers(int n){
+        int sum = 0;
+
+        for(int i = n; i > 0; i--){
+            sum += i;
+        }
+        return sum;
     }
 }
